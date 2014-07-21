9f6fbebe5d3d02ee29d2a371e6cbcfcb3f2105e9
penyu <penyu@amazon.com>
2014-07-21 04:19:23 +0000

    remove insert/delete code

--- server.py
+++ server.py
@@ -86,59 +86,6 @@ def upload():
     status_and_info = '%s\n%s' % (status, info)
     return render_template('upload.html', status_and_info=status_and_info)
 
-@app.route('/insert', methods=['GET', 'POST'])
-def insert():
-    if request.method == 'POST':
-        insert_file = request.files['insert_file']
-        if not insert_file:
-            return 'no insert file'
-        filename = secure_filename(insert_file.filename)
-        timestamp = '%f' % time.time()
-        insert_filename = '%s.%s' % (timestamp, filename)
-        insert_filename = os.path.join(current_file_full_path, upload_folder, insert_filename)
-        insert_file.save(insert_filename)
-        value = request.form.getlist('update')
-        if value:
-            action = 'update'
-        else:
-            action = 'create'
-        try:
-            send_message(action, insert_filename)
-        except Exception, e:
-            os.remove(insert_filename)
-            return unicode(e)
-        os.remove(insert_filename)
-        return redirect(url_for('insert'))
-    (status, info) = get_information()
-    status_and_info = '%s\n%s' % (status, info)
-    return render_template('insert.html', status_and_info=status_and_info)
-
-@app.route('/delete', methods=['GET', 'POST'])
-def delete():
-    if request.method == 'POST':
-        delete_file = request.files['delete_file']
-        if not delete_file:
-            return 'no delete file'
-        filename = secure_filename(delete_file.filename)
-        timestamp = '%f' % time.time()
-        delete_filename = '%s.%s' % (timestamp, filename)
-        delete_filename = os.path.join(upload_folder, delete_filename)
-        delete_file.save(delete_filename)
-        try:
-            send_message('delete', delete_filename)
-        except Exception, e:
-            os.remove(delete_filename)
-            return unicode(e)
-        os.remove(delete_filename)
-        return redirect(url_for('delete'))
-    return render_template('delete.html')
-
-@app.route('/status', methods=['GET'])
-def status():
-    (status, info) = get_information()
-    status_and_info = '%s\n%s' % (status, info)
-    return status_and_info
-
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     return 'search'
