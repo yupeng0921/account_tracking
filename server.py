a7bc9b9835662333484e04d7f58709c3b7dc397b
penyu <penyu@amazon.com>
2014-08-16 12:21:00 +0000

    redirect to where want to access

--- server.py
+++ server.py
@@ -96,7 +96,7 @@ def login():
             logging.info(registeredUser.password)
         if registeredUser != None and unicode(registeredUser.password) == unicode(password):
             login_user(registeredUser)
-            return redirect(url_for('index'))
+            return redirect(request.args.get("next") or url_for("index"))
         else:
             logging.warning('invalide username or password: %s %s' % (username, password))
             return abort(401)
